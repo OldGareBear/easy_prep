205a663e54c5c9e53817de291d49f20d0857ab6d
Gary Bethea <gbethea@mdsol.com>
2019-10-14 18:25:48 -0400

    set up file attachment for tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,6 @@ class Test < ApplicationRecord
   belongs_to :creator, class: 'User', foreign_key: :creator_id
   has_many :test_assignments
   has_many :questions
+
+  has_attached_file :document
 end
