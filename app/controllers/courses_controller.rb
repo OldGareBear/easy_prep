ca8b0fadb8d6fb5f4022894374ad5e315d367fe3
Gary Bethea <gbethea@mdsol.com>
2019-10-15 19:01:35 -0400

    add ability to add students to class

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -12,11 +12,10 @@ class CoursesController < ApplicationController
   end
 
   def create
-    byebug
     @course = Course.new(new_course_params.merge(teacher: current_user))
 
     respond_to do |format|
-      if @course.save
+      if @course.save && transactionally_save_students
         format.html { redirect_to @course, notice: 'Course was successfully created.' }
         format.json { render :show, status: :created, location: @todo_list }
       else
@@ -31,7 +30,44 @@ class CoursesController < ApplicationController
 
   private
 
+  def transactionally_save_students
+    students =  []
+    User.transaction do
+      params[:students].each do  |_, student_params|
+        first_name, last_name = student_params[:first_name].downcase, student_params[:last_name].downcase
+        students << create_student!(first_name, last_name)
+      end
+    end
+    @course.students << students
+  end
+
+  def generate_fake_email(first_name, last_name)
+    email_candidate_search = first_name.chars.first + last_name + '%@easy_prep.student'
+    matches = User.where("email like ?", email_candidate_search).pluck(:email)
+    highest_increment = matches.map { |match| match.match(/\d/).to_a.first.to_i }.max
+    first_name.chars.first + last_name + increment(highest_increment, matches.count)  + '@easy_prep.student'
+  end
+
+  def generate_fake_password(first_name, last_name)
+    first_name.chars.first + last_name + '-password'
+  end
+
+  def increment(highest_increment, num_matches)
+    return '' if num_matches == 0
+    increment = highest_increment ? highest_increment + 1 : 2
+    increment.to_s
+  end
+
   def new_course_params
     params.require(:course).permit(:name, :grade)
+  end
+
+  def create_student!(first_name, last_name)
+    User.create_student!(
+      first_name: first_name,
+      last_name: last_name,
+      email: generate_fake_email(first_name, last_name),
+      password: generate_fake_password(first_name, last_name)
+    )
   end
 end
--- app/models/user_type.rb
+++ app/models/user_type.rb
@@ -1,13 +1,22 @@
 class UserType < ApplicationRecord
   TEACHER_NAME = 'teacher'.freeze
+  STUDENT_NAME = 'student'.freeze
 
   has_many :users
 
   def self.teacher
-    find_by_name(TEACHER_NAME)
+    find_or_create_by(name: TEACHER_NAME)
+  end
+
+  def self.student
+    find_or_create_by(name: STUDENT_NAME)
   end
 
   def teacher?
     name == TEACHER_NAME
+  end
+
+  def student?
+    name == STUDENT_NAME
   end
 end
