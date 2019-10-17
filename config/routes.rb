35e51bd10959e82981857d623e2836dfee5b3763
Gary Bethea <gbethea@mdsol.com>
2019-10-17 09:02:39 -0400

    more header stuff

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :courses
+
+  resources :users, only: [:edit, :show, :update]
 end
