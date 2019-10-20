f42c1a271db178a9f2ec18aca1380cb2834d00c5
Gary Bethea <gbethea@mdsol.com>
2019-10-20 17:00:23 -0400

    cool checkbox whattup

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     (function () {
+        // Enabled tabs
         $('.menu .item').tab();
+
+        // Enabled cool checkboxes;
+        $('.ui.checkbox').checkbox();
 
         $('#add-question').click(function() {
             $('.ui.modal').modal('show');
