f77924722bb083e2d75449bc76055586ef2b9ba4
Gary Bethea <gbethea@mdsol.com>
2019-10-20 18:33:44 -0400

    start on javascript to new answer options

---
+++ app/assets/javascripts/test_assignments.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    (function () {
+        var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+        // Enabled tabs
+        $('.menu .item').tab();
+
+        // Enabled cool checkboxes;
+        $('.ui.checkbox').checkbox();
+    }());
+}, false);
--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -1,11 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
     (function () {
-        // Enabled tabs
-        $('.menu .item').tab();
-
-        // Enabled cool checkboxes;
-        $('.ui.checkbox').checkbox();
-
         $('#add-question').click(function() {
             $('.ui.modal').modal('show');
         });
