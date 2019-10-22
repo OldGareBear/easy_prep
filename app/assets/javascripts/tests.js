eb08237bb78058da3bc9660c29dad9f93546cbad
Gary Bethea <gbethea@mdsol.com>
2019-10-22 09:18:57 -0400

    add javascript to add questions to test

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -5,7 +5,18 @@ document.addEventListener('DOMContentLoaded', function(){
         });
 
         $('#save-question').click(function () {
+            // copy question to test
+            var question = $('.ui.bottom.attached.tab.segment.active').find('.question.ui.container')[0];
+            var clone = $.clone(question);
+            $(clone).addClass('segment');
+            $(clone).addClass('item');
+            $('#questions-container').append(clone);
+            $('.ui.modal').modal('hide');
 
+            // clear inputs
+            var modalTabs = $('.ui.bottom.attached.tab');
+            modalTabs.find('input').val('');
+            modalTabs.find('textarea').val('');
         });
     }());
 }, false);
