b2c2dc2e22e03434b56df3fdd4929f901c9ec167
Gary Bethea <gbethea@mdsol.com>
2019-10-22 18:55:02 -0400

    start working on supporting multiple questions

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -15,16 +15,18 @@ document.addEventListener('DOMContentLoaded', function(){
             $('#questions-container').append(clone);
             $('.ui.modal').modal('hide');
 
-            // set question input names based on number of questions
+            // increment question count
+            window._easy_prep_number_of_questions = window._easy_prep_number_of_questions + 1;
 
+            // set question input names based on number of questions
+            question
 
             // clear inputs
             var modalTabs = $('.ui.bottom.attached.tab');
             modalTabs.find('input').val('');
             modalTabs.find('textarea').val('');
-
-            // increment question count
-            window._easy_prep_number_of_questions = window._easy_prep_number_of_questions + 1;
+            modalTabs.find('.ui.toggle.checkbox.checked').removeClass('checked');
+            $('.ui.toggle.checkbox.checked').find('input').prop('checked', false)
         });
     }());
 }, false);
