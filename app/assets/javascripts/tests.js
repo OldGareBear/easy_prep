771574c3e9dd99f98522fa2afa983a2b060c0dec
Gary Bethea <gbethea@mdsol.com>
2019-10-23 09:13:53 -0400

    keep going almost there, sort of

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     (function () {
-        window._easy_prep_number_of_questions = 0;
+        window._easy_prep_question_number = 0;
 
         $('#add-question').click(function() {
             $('.ui.modal').modal('show');
@@ -16,18 +16,28 @@ document.addEventListener('DOMContentLoaded', function(){
             $('.ui.modal').modal('hide');
 
             // increment question count
-            window._easy_prep_number_of_questions = window._easy_prep_number_of_questions + 1;
+            window._easy_prep_question_number = window._easy_prep_question_number + 1;
 
             // set question input names based on number of questions
-            var newDescription = 'question[' + window._easy_prep_number_of_questions + '][description]';
-            $(question).find('#question_description').prop('name', newDescription);
+            var newDescription = 'question[' + window._easy_prep_question_number + '][description]';
+            var modalContainer = $('ui.bottom.attached.tab.segment');
+            modalContainer.find('#question_description').prop('name', newDescription);
+            var previousQuestion = window._easy_prep_question_number - 1;
+            var a = modalContainer.find("input[name='question[" + previousQuestion + "][answer_option][0]']");
+            var b = modalContainer.find("input[name='question[" + previousQuestion + "][answer_option][1]']");
+            var c = modalContainer.find("input[name='question[" + previousQuestion + "][answer_option][2]']");
+            var d = modalContainer.find("input[name='question[" + previousQuestion + "][answer_option][3]']");
+            a.prop('name', 'question[' + window._easy_prep_question_number + '][answer_option][0]');
+            b.prop('name', 'question[' + window._easy_prep_question_number + '][answer_option][1]');
+            c.prop('name', 'question[' + window._easy_prep_question_number + '][answer_option][2]');
+            d.prop('name', 'question[' + window._easy_prep_question_number + '][answer_option][3]');
 
             // clear inputs
             var modalTabs = $('.ui.bottom.attached.tab');
             modalTabs.find('input').val('');
             modalTabs.find('textarea').val('');
             modalTabs.find('.ui.toggle.checkbox.checked').removeClass('checked');
-            $('.ui.toggle.checkbox.checked').find('input').prop('checked', false)
+            $('.ui.toggle.checkbox').find('input').prop('checked', false)
         });
     }());
 }, false);
