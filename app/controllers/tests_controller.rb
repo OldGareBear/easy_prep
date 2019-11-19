85f95b00366619d4a67db42abd2467d947c6460e
Gary Bethea <gbethea@mdsol.com>
2019-11-19 08:17:23 -0500

    get tables working

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -29,9 +29,9 @@ class TestsController < ApplicationController
 
     @score_data = TestAssignment
                     .connection
-                    .execute("""SELECT total.name, total.created_at, (COALESCE(correct_answers, 0) / cast(total_questions as decimal) * 100) as score
+                    .execute("""SELECT total.student_id, (COALESCE(correct_answers, 0) / cast(total_questions as decimal) * 100) as score
                                 FROM (
-                                  SELECT tests.name, tests.created_at, COUNT(test_assignment_questions.id) AS total_questions
+                                  SELECT test_assignments.student_id, COUNT(test_assignment_questions.id) AS total_questions
                                   FROM test_assignments
                                   JOIN tests ON tests.id = test_assignments.test_id
                                   JOIN test_assignment_questions ON test_assignment_questions.test_assignment_id = test_assignments.id
@@ -40,10 +40,9 @@ class TestsController < ApplicationController
                                   WHERE test_assignments.course_id = #{@course.id}
                                   AND test_assignments.graded_at IS NOT NULL
                                   AND test_assignments.test_id = #{@test.id}
-                                  GROUP BY tests.name, tests.created_at
-                                  ORDER BY tests.created_at ASC
+                                  GROUP BY test_assignments.student_id
                                 ) total LEFT OUTER JOIN (
-                                  SELECT tests.name, tests.created_at, COUNT(test_assignment_questions.id) AS correct_answers
+                                  SELECT test_assignments.student_id, COUNT(test_assignment_questions.id) AS correct_answers
                                   FROM test_assignments
                                   JOIN tests ON tests.id = test_assignments.test_id
                                   JOIN test_assignment_questions ON test_assignment_questions.test_assignment_id = test_assignments.id
@@ -53,46 +52,47 @@ class TestsController < ApplicationController
                                   AND test_assignments.graded_at IS NOT NULL
                                   AND answer_options.correct = true
                                   AND test_assignments.test_id = #{@test.id}
-                                  GROUP BY tests.name, tests.created_at
-                                  ORDER BY tests.created_at ASC
-                                ) correct ON correct.name  = total.name""")
-                    .to_a.map { |hash| [hash['name'], hash['score']] }.to_h
+                                  GROUP BY test_assignments.student_id
+                                ) correct ON correct.student_id  = total.student_id""")
+                    .to_a.map { |hash| [User.find(hash['student_id']).name, hash['score']] }.to_h
 
-    @skill_data = TestAssignment
-                    .connection
-                    .execute("""SELECT total.oid, total.skill_id, total_questions - correct_answers AS incorrect_answers, correct_answers
-                                FROM (
-                                  SELECT skills.oid, skills.id as skill_id, COUNT(test_assignment_questions.id) AS total_questions
-                                  FROM test_assignments
-                                  JOIN tests ON tests.id = test_assignments.test_id
-                                  JOIN test_assignment_questions ON test_assignment_questions.test_assignment_id = test_assignments.id
-                                  JOIN questions ON questions.id = test_assignment_questions.question_id
-                                  JOIN skills ON skills.id = questions.skill_id
-                                  JOIN answer_options ON answer_options.id = test_assignment_questions.answer_id
-                                  WHERE test_assignments.course_id = #{@course.id}
-                                  AND test_assignments.graded_at IS NOT NULL
-                                  AND test_assignments.test_id = #{@test.id}
-                                  GROUP BY skills.oid, skills.id
-                                ) total LEFT OUTER JOIN (
-                                  SELECT skills.oid, skills.id as skill_id, COUNT(test_assignment_questions.id) AS correct_answers
-                                  FROM test_assignments
-                                  JOIN tests ON tests.id = test_assignments.test_id
-                                  JOIN test_assignment_questions ON test_assignment_questions.test_assignment_id = test_assignments.id
-                                  JOIN questions ON questions.id = test_assignment_questions.question_id
-                                  JOIN skills ON skills.id = questions.skill_id
-                                  JOIN answer_options ON answer_options.id = test_assignment_questions.answer_id
-                                  WHERE test_assignments.course_id = #{@course.id}
-                                  AND test_assignments.graded_at IS NOT NULL
-                                  AND answer_options.correct = true
-                                  AND test_assignments.test_id = #{@test.id}
-                                  GROUP BY skills.oid, skills.id
-                                ) correct ON correct.oid  = total.oid""")
-                    .to_a.map do |skill| {
-                      oid: skill['oid'],
-                      skill_id: skill['skill_id'],
-                      incorrect_answers: skill['incorrect_answers'],
-                      correct_answers: skill['correct_answers']
-                    }
+    skill_data = TestAssignment
+                   .connection
+                   .execute("""SELECT total.oid, total.skill_id, total_questions - correct_answers AS incorrect_answers, correct_answers
+                               FROM (
+                                 SELECT skills.oid, skills.id as skill_id, COUNT(test_assignment_questions.id) AS total_questions
+                                 FROM test_assignments
+                                 JOIN tests ON tests.id = test_assignments.test_id
+                                 JOIN test_assignment_questions ON test_assignment_questions.test_assignment_id = test_assignments.id
+                                 JOIN questions ON questions.id = test_assignment_questions.question_id
+                                 JOIN skills ON skills.id = questions.skill_id
+                                 JOIN answer_options ON answer_options.id = test_assignment_questions.answer_id
+                                 WHERE test_assignments.course_id = #{@course.id}
+                                 AND test_assignments.graded_at IS NOT NULL
+                                 AND test_assignments.test_id = #{@test.id}
+                                 GROUP BY skills.oid, skills.id
+                               ) total LEFT OUTER JOIN (
+                                 SELECT skills.oid, skills.id as skill_id, COUNT(test_assignment_questions.id) AS correct_answers
+                                 FROM test_assignments
+                                 JOIN tests ON tests.id = test_assignments.test_id
+                                 JOIN test_assignment_questions ON test_assignment_questions.test_assignment_id = test_assignments.id
+                                 JOIN questions ON questions.id = test_assignment_questions.question_id
+                                 JOIN skills ON skills.id = questions.skill_id
+                                 JOIN answer_options ON answer_options.id = test_assignment_questions.answer_id
+                                 WHERE test_assignments.course_id = #{@course.id}
+                                 AND test_assignments.graded_at IS NOT NULL
+                                 AND answer_options.correct = true
+                                 AND test_assignments.test_id = #{@test.id}
+                                 GROUP BY skills.oid, skills.id
+                              ) correct ON correct.oid  = total.oid""")
+
+    @skill_data = skill_data.to_a.map do |skill|
+      {
+        oid: skill['oid'],
+        skill_id: skill['skill_id'],
+        incorrect_answers: skill['incorrect_answers'],
+        correct_answers: skill['correct_answers']
+      }
     end
   end
 
